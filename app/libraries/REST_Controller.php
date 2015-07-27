4265b70f7076670f7f6ee2af09a6379e4741c501
nosun <nosun2008@126.com>
2015-07-27 09:20:10 +0800

    del some http code that not use

--- app/libraries/REST_Controller.php
+++ app/libraries/REST_Controller.php
@@ -14,16 +14,6 @@
  */
 
 abstract class REST_Controller extends CI_Controller {
-    // Note: Only the widely used HTTP status codes are documented
-
-    // Informational
-
-    const HTTP_CONTINUE = 100;
-    const HTTP_SWITCHING_PROTOCOLS = 101;
-    const HTTP_PROCESSING = 102;            // RFC2518
-
-    // Success
-
     /**
      * The request has succeeded
      */
@@ -36,31 +26,12 @@ abstract class REST_Controller extends CI_Controller {
     const HTTP_ACCEPTED = 202;
     const HTTP_NON_AUTHORITATIVE_INFORMATION = 203;
 
-    /**
-     * The server successfully processed the request, though no content is returned
-     */
-    const HTTP_NO_CONTENT = 204;
-    const HTTP_RESET_CONTENT = 205;
-    const HTTP_PARTIAL_CONTENT = 206;
-    const HTTP_MULTI_STATUS = 207;          // RFC4918
-    const HTTP_ALREADY_REPORTED = 208;      // RFC5842
-    const HTTP_IM_USED = 226;               // RFC3229
-
     // Redirection
 
     const HTTP_MULTIPLE_CHOICES = 300;
     const HTTP_MOVED_PERMANENTLY = 301;
     const HTTP_FOUND = 302;
     const HTTP_SEE_OTHER = 303;
-
-    /**
-     * The resource has not been modified since the last request
-     */
-    const HTTP_NOT_MODIFIED = 304;
-    const HTTP_USE_PROXY = 305;
-    const HTTP_RESERVED = 306;
-    const HTTP_TEMPORARY_REDIRECT = 307;
-    const HTTP_PERMANENTLY_REDIRECT = 308;  // RFC7238
 
     // Client Error
 
@@ -97,61 +68,18 @@ abstract class REST_Controller extends CI_Controller {
      * The request was not acceptable
      */
     const HTTP_NOT_ACCEPTABLE = 406;
-    const HTTP_PROXY_AUTHENTICATION_REQUIRED = 407;
-    const HTTP_REQUEST_TIMEOUT = 408;
-
-    /**
-     * The request could not be completed due to a conflict with the current state
-     * of the resource
-     */
-    const HTTP_CONFLICT = 409;
-    const HTTP_GONE = 410;
-    const HTTP_LENGTH_REQUIRED = 411;
-    const HTTP_PRECONDITION_FAILED = 412;
-    const HTTP_REQUEST_ENTITY_TOO_LARGE = 413;
-    const HTTP_REQUEST_URI_TOO_LONG = 414;
-    const HTTP_UNSUPPORTED_MEDIA_TYPE = 415;
-    const HTTP_REQUESTED_RANGE_NOT_SATISFIABLE = 416;
-    const HTTP_EXPECTATION_FAILED = 417;
-    const HTTP_I_AM_A_TEAPOT = 418;                                               // RFC2324
-    const HTTP_UNPROCESSABLE_ENTITY = 422;                                        // RFC4918
-    const HTTP_LOCKED = 423;                                                      // RFC4918
-    const HTTP_FAILED_DEPENDENCY = 424;                                           // RFC4918
-    const HTTP_RESERVED_FOR_WEBDAV_ADVANCED_COLLECTIONS_EXPIRED_PROPOSAL = 425;   // RFC2817
-    const HTTP_UPGRADE_REQUIRED = 426;                                            // RFC2817
-    const HTTP_PRECONDITION_REQUIRED = 428;                                       // RFC6585
     const HTTP_TOO_MANY_REQUESTS = 429;                                           // RFC6585
-    const HTTP_REQUEST_HEADER_FIELDS_TOO_LARGE = 431;                             // RFC6585
 
     // Server Error
-
-    /**
-     * The server encountered an unexpected error
-     *
-     * Note: This is a generic error message when no specific message
-     * is suitable
-     */
     const HTTP_INTERNAL_SERVER_ERROR = 500;
 
-    /**
-     * The server does not recognise the request method
+    /*
+     *
+     * This defines the rest format.
+     * Must be overridden it in a controller so that it is set.
+     *
+     * @var string|NULL
      */
-    const HTTP_NOT_IMPLEMENTED = 501;
-    const HTTP_BAD_GATEWAY = 502;
-    const HTTP_SERVICE_UNAVAILABLE = 503;
-    const HTTP_GATEWAY_TIMEOUT = 504;
-    const HTTP_VERSION_NOT_SUPPORTED = 505;
-    const HTTP_VARIANT_ALSO_NEGOTIATES_EXPERIMENTAL = 506;                        // RFC2295
-    const HTTP_INSUFFICIENT_STORAGE = 507;                                        // RFC4918
-    const HTTP_LOOP_DETECTED = 508;                                               // RFC5842
-    const HTTP_NOT_EXTENDED = 510;                                                // RFC2774
-    const HTTP_NETWORK_AUTHENTICATION_REQUIRED = 511;
-    /**
- * This defines the rest format.
- * Must be overridden it in a controller so that it is set.
- *
- * @var string|NULL
- */
     protected $rest_format = 'json';
 
     /**
@@ -159,7 +87,8 @@ abstract class REST_Controller extends CI_Controller {
      *
      * @var array
      */
-    protected $methods = [];
+
+    //protected $methods = [];
 
     /**
      * List of allowed HTTP methods
@@ -185,15 +114,6 @@ abstract class REST_Controller extends CI_Controller {
      * @var object
      */
     protected $response = NULL;
-
-    /**
-     * Contains details about the REST API
-     * Fields: db, ignore_limits, key, level, user_id
-     * Note: This is a dynamic object (stdClass)
-     *
-     * @var object
-     */
-    protected $rest = NULL;
 
     /**
      * The arguments for the GET request method
@@ -259,34 +179,6 @@ abstract class REST_Controller extends CI_Controller {
     protected $_args = [];
 
     /**
-     * If the request is allowed based on the API key provided.
-     *
-     * @var bool
-     */
-    protected $_allow = TRUE;
-
-    /**
-     * Determines if output compression is enabled
-     *
-     * @var bool
-     */
-    protected $_zlib_oc = FALSE;
-
-    /**
-     * The start of the response time from the server
-     *
-     * @var string
-     */
-    protected $_start_rtime = '';
-
-    /**
-     * The end of the response time from the server
-     *
-     * @var string
-     */
-    protected $_end_rtime = '';
-
-    /**
      * List all supported methods, the first will be the default format
      *
      * @var array
@@ -300,13 +192,6 @@ abstract class REST_Controller extends CI_Controller {
         'serialized' => 'application/vnd.php.serialized',
         'xml' => 'application/xml'
     ];
-
-    /**
-     * Information about the current API user
-     *
-     * @var object
-     */
-    protected $_apiuser;
 
     /**
      * Enable XSS flag
@@ -362,9 +247,6 @@ abstract class REST_Controller extends CI_Controller {
         // Initialise the response, request and rest objects
         $this->request = new stdClass();
         $this->response = new stdClass();
-        $this->rest = new stdClass();
-
-        $this->_zlib_oc = @ini_get('zlib.output_compression');
 
         // Determine whether the connection is HTTPS
         $this->request->ssl = is_https();
@@ -570,12 +452,13 @@ abstract class REST_Controller extends CI_Controller {
      * @access private
      * @return string|NULL Supported input format; otherwise, NULL
      */
+    // 检测input的格式，比如 json，xml，与系统允许的格式进行比对，返回格式的key。
+    // 一般来说是 application/x-www-form-urlencoded
     private function _detect_input_format()
     {
         // Get the CONTENT-TYPE value from the SERVER variable
         // default form post => application/x-www-form-urlencoded
         $contentType = $this->input->server('CONTENT_TYPE');
-        //var_dump($contentType);
 
         if (empty($contentType) === FALSE)
         {
